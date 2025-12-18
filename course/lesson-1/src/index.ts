92ec78430cd1957fe7fddda60a616556b3b6b70d
Your Name <you@example.com>
2025-12-18 09:24:39 +0800

    code for course 1

--- course/lesson-1/src/index.ts
+++ course/lesson-1/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, TypeScript!");
+import { ethers } from "ethers";
+
+async function main() {
+    const URL = "https://services.polkadothub-rpc.com/testnet";
+    const provider = new ethers.JsonRpcProvider(URL);
+    const address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"; // put your address here
+    const balance = await provider.getBalance(address);
+    console.log(`Balance of ${address}: ${ethers.formatEther(balance)} ETH`);
+}
+
+main();
